8dadd508e50ec53dc046ad185197576d7eefa41c
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-07-17 15:18:10 +0200

    simplify

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
 import "katex/dist/katex.css";
-import "prismjs/themes/prism-okaidia.css"
+import "prismjs/themes/prism-okaidia.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; /* eslint-disable import/first */
-
-import "./src/styles/global.css";
+import "tailwindcss/tailwind.css";
