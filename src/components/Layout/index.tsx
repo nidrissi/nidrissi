afd54de5d8b13aaa23b3f7145f1720f136c57400
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-07-22 18:47:36 +0200

    add more padding

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -15,7 +15,7 @@ export default function Layout({ children, lang, ...props }: LayoutProps) {
     <div className="flex flex-col min-h-screen dark:bg-gray-800 dark:text-gray-300">
       <SEO {...props} />
       <Navbar />
-      <main className="container mx-auto mb-4 px-2 flex-grow" lang={lang}>
+      <main className="container mx-auto mb-4 px-4 flex-grow" lang={lang}>
         {children}
       </main>
       <Cookie />
