d20e4d6ba88814704d9747a43064b8e4d765463c
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-07-31 20:23:05 +0200

    simplification

--- api/GetComments.cs
+++ api/GetComments.cs
@@ -4,10 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
-using System;
 using System.Linq;
-using System.Text.Json;
 
 namespace Idrissi.Blogging
 {
@@ -18,25 +15,26 @@ namespace Idrissi.Blogging
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "comment/{pageId}")]
             HttpRequest req,
             string pageId,
-            [CosmosDB(databaseName: "Blogging", collectionName: "Comments",
-                ConnectionStringSetting = "CosmosDbConnectionString")]
+            [CosmosDB(ConnectionStringSetting = "CosmosDbConnectionString")]
             DocumentClient client,
             ILogger log)
         {
             if (string.IsNullOrWhiteSpace(pageId))
             {
-                return new NotFoundResult();
+                log.LogWarning("Empty request, aborting.");
+                return new BadRequestResult();
             }
-            log.LogInformation($"Fetching comments for {pageId}");
 
-            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("Blogging", "Comments");
+            log.LogInformation("Fetching comments for {pageId}", pageId);
 
-            var comments = from c in client.CreateDocumentQuery<Comment>(collectionUri)
+            var commentsCollectionUri = UriFactory.CreateDocumentCollectionUri("Blogging", "Comments");
+
+            var comments = from c in client.CreateDocumentQuery<Comment>(commentsCollectionUri)
                            where c.pageId == pageId
                            orderby c.date descending
                            select c;
-            var body = JsonSerializer.Serialize(comments);
-            return new OkObjectResult(body);
+
+            return new OkObjectResult(comments);
         }
     }
 }
