0c14b245945db9097b162039a5bfceb278cb8d89
Najib Idrissi <najib.idrissi.kaitouni@outlook.com>
2021-08-03 10:30:16 +0200

    fix int => long

--- api/Comment.cs
+++ api/Comment.cs
@@ -1,10 +1,12 @@
+using System;
+
 namespace Idrissi.Blogging
 {
     public class Comment
     {
         public string id { get; set; }
         public string pageId { get; set; }
-        public int timestamp { get; set; }
+        public long timestamp { get; set; }
         public string content { get; set; }
         public string authorId { get; set; }
         public string authorName { get; set; }
